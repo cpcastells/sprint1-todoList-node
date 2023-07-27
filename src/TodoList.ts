f5759be636267a389bd51ca4d2de1edbbe18bfd4
Carles Pueyo <cpcastells@gmail.com>
2023-07-27 12:04:53 +0200

    Create TodoList class with add() method

---
+++ src/TodoList.ts
@@ -0,0 +1,22 @@
+import { v4 as uuidv4 } from "uuid";
+import type Task from "./types";
+
+class TodoList {
+  private readonly tasks: Task[];
+
+  constructor() {
+    this.tasks = [];
+  }
+
+  addTask = (description: string) => {
+    const newTask = {
+      id: uuidv4(),
+      description,
+      completed: false,
+    };
+    this.tasks.push(newTask);
+    return newTask;
+  };
+}
+
+export default TodoList;
